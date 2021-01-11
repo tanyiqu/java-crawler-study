7219552258e47c4d6abd437aaa4cb00f708468bc
Tanyiqu <1953649096@qq.com>
2021-01-11 15:20:25 +0800

    Jsoup 获取dom元素

---
+++ java-crawler-study/02-jsoup/src/main/java/tanyiqu/crawler/Demo_02_Jsoup_dom.java
@@ -0,0 +1,78 @@
+package tanyiqu.crawler;
+
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class Demo_02_Jsoup_dom {
+
+    @Test
+    public void getElementById() throws IOException {
+        // 从url获取document
+        Document doc = Jsoup.parse(new URL("http://www.tanyiqu.top/"), 5000);
+
+        Element input = doc.getElementById("input");
+
+        System.out.println(input.attr("placeholder"));
+    }
+
+    @Test
+    public void getElementByTag() throws IOException {
+        // 从url获取document
+        Document doc = Jsoup.parse(new URL("http://www.tanyiqu.top/"), 5000);
+
+        Elements list = doc.getElementsByTag("a");
+
+        for (Element a : list) {
+            System.out.println(a.text() + "：" + a.attr("href"));
+        }
+    }
+
+    @Test
+    public void getElementByClass() throws IOException {
+        // 从url获取document
+        Document doc = Jsoup.parse(new URL("http://www.tanyiqu.top/"), 5000);
+
+        Elements elements = doc.getElementsByClass("ul2");
+
+        for (Element element : elements) {
+            System.out.println(element.text());
+        }
+    }
+
+    @Test
+    public void getElementByAttribute() throws IOException {
+        // 从url获取document
+        Document doc = Jsoup.parse(new URL("http://www.tanyiqu.top/"), 5000);
+
+        Elements elements = doc.getElementsByAttribute("type");
+
+        for (Element element : elements) {
+            System.out.println(element.attr("type"));
+        }
+    }
+
+    @Test
+    public void getElementByAttributeValue() throws IOException {
+        // 从url获取document
+        Document doc = Jsoup.parse(new URL("http://www.tanyiqu.top/"), 5000);
+
+        Elements elements = doc.getElementsByAttributeValue("type", "search");
+
+        for (Element element : elements) {
+            System.out.println(element.attr("type"));
+        }
+    }
+
+
+}
+
+
+
+
